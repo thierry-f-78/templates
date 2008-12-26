48d2e50001b6c6b9479db2d3e1a85bea4398250a
thierry <thierry@athlonx2.thierry.local>
2008-12-26 18:32:19 +0100

    CLEANUP: collapse des fonctions a deux operandes et reduction des switch/case

--- exec_run.c
+++ exec_run.c
@@ -54,20 +54,20 @@
 		case X_NULL: goto exec_X_NULL; \
 		case X_COLLEC: goto exec_X_COLLEC; \
 		case X_PRINT: goto exec_X_PRINT; \
-		case X_ADD: goto exec_X_ADD; \
-		case X_SUB: goto exec_X_SUB; \
-		case X_MUL: goto exec_X_MUL; \
-		case X_DIV: goto exec_X_DIV; \
-		case X_MOD: goto exec_X_MOD; \
-		case X_EQUAL: goto exec_X_EQUAL; \
-		case X_STREQ: goto exec_X_STREQ; \
-		case X_DIFF: goto exec_X_DIFF; \
-		case X_LT: goto exec_X_LT; \
-		case X_GT: goto exec_X_GT; \
-		case X_LE: goto exec_X_LE; \
-		case X_GE: goto exec_X_GE; \
-		case X_AND: goto exec_X_AND; \
-		case X_OR: goto exec_X_OR; \
+		case X_ADD: \
+		case X_SUB: \
+		case X_MUL: \
+		case X_DIV: \
+		case X_MOD: \
+		case X_EQUAL: \
+		case X_STREQ: \
+		case X_DIFF: \
+		case X_LT: \
+		case X_GT: \
+		case X_LE: \
+		case X_GE: \
+		case X_AND: \
+		case X_OR: goto exec_X_TWO_OPERAND; \
 		case X_ASSIGN: goto exec_X_ASSIGN; \
 		case X_DISPLAY: goto exec_X_DISPLAY; \
 		case X_FUNCTION: goto exec_X_FUNCTION; \
@@ -93,18 +93,28 @@
 
 #define switchline(index) case index: goto exec_ ## index;
 
+/* for updating this function, run :r !./get_return_points.sh */
 #define exec_return() \
 	switch (egt(-2).ent) { \
-		switchline(1)  switchline(2)  switchline(3)  switchline(4)  switchline(5) \
-		switchline(6)  switchline(7)  switchline(8)  switchline(9)  switchline(10) \
-		switchline(11) switchline(12) switchline(13) switchline(14) switchline(15) \
-		switchline(16) switchline(17) switchline(18) switchline(19) switchline(20) \
-		switchline(21) switchline(22) switchline(23) switchline(24) switchline(25) \
-		switchline(26) switchline(27) switchline(28) switchline(29) switchline(30) \
-		switchline(31) switchline(32) switchline(33) switchline(34) switchline(35) \
-		switchline(36) switchline(37) switchline(38) switchline(39) switchline(40) \
-		switchline(41) switchline(42) switchline(43) switchline(44) \
 		default: \
+		switchline(1) \
+		switchline(2) \
+		switchline(3) \
+		switchline(4) \
+		switchline(5) \
+		switchline(30) \
+		switchline(31) \
+		switchline(32) \
+		switchline(33) \
+		switchline(34) \
+		switchline(35) \
+		switchline(36) \
+		switchline(37) \
+		switchline(38) \
+		switchline(39) \
+		switchline(40) \
+		switchline(41) \
+		switchline(42) \
 			snprintf(r->e->error, ERROR_LEN, "[%s:%d] error in return code", __FILE__, __LINE__); \
 			return -1; \
 	}
@@ -268,9 +278,22 @@ int exec_run_now(struct exec_run *r) {
 /**********************************************************************
 * 
 * X_ADD
+* X_SUB
+* X_MUL
+* X_DIV
+* X_MOD
+* X_EQUAL
+* X_STREQ
+* X_DIFF
+* X_LT
+* X_GT
+* X_LE
+* X_GE
+* X_AND
+* X_OR
 *
 **********************************************************************/
-	exec_function(X_ADD) {
+	exec_function(X_TWO_OPERAND) {
 		/* -3 : n
 		 * -2 : a et val(a)
 		 * -1 : b et val(b)
@@ -283,319 +306,42 @@ int exec_run_now(struct exec_run *r) {
 		exec_NODE(egt(-2).n, 4, egt(-2).ptr);
 		exec_NODE(egt(-1).n, 5, egt(-1).ptr);
 
-		egt(-3).ent = egt(-2).ent + egt(-1).ent;
-		exec_free_stack(2);
-		exec_return();
-	} end_function
-
-
-/**********************************************************************
-* 
-* X_SUB
-*
-**********************************************************************/
-	exec_function(X_SUB) {
-		/* -3 : n
-		 * -2 : a et val(a)
-		 * -1 : b et val(b)
-		 */
-		exec_reserve_stack(2);
-
-		egt(-2).n = container_of(egt(-3).n->c.next, struct exec_node, b);
-		egt(-1).n = container_of(egt(-2).n->b.next, struct exec_node, b);
-
-		exec_NODE(egt(-2).n, 6, egt(-2).ptr);
-		exec_NODE(egt(-1).n, 7, egt(-1).ptr);
-
-		egt(-3).ent = egt(-2).ent - egt(-1).ent;
-		exec_free_stack(2);
-		exec_return();
-	} end_function
-
-
-/**********************************************************************
-* 
-* X_MUL
-*
-**********************************************************************/
-	exec_function(X_MUL) {
-		/* -3 : n
-		 * -2 : a et val(a)
-		 * -1 : b et val(b)
-		 */
-		exec_reserve_stack(2);
-
-		egt(-2).n = container_of(egt(-3).n->c.next, struct exec_node, b);
-		egt(-1).n = container_of(egt(-2).n->b.next, struct exec_node, b);
-
-		exec_NODE(egt(-2).n, 8, egt(-2).ptr);
-		exec_NODE(egt(-1).n, 9, egt(-1).ptr);
-
-		egt(-3).ent = egt(-2).ent * egt(-1).ent;
-		exec_free_stack(2);
-		exec_return();
-	} end_function
-
-
-/**********************************************************************
-* 
-* X_DIV
-*
-**********************************************************************/
-	exec_function(X_DIV) {
-		/* -3 : n
-		 * -2 : a et val(a)
-		 * -1 : b et val(b)
-		 */
-		exec_reserve_stack(2);
-
-		egt(-2).n = container_of(egt(-3).n->c.next, struct exec_node, b);
-		egt(-1).n = container_of(egt(-2).n->b.next, struct exec_node, b);
-
-		exec_NODE(egt(-2).n, 10, egt(-2).ptr);
-		exec_NODE(egt(-1).n, 11, egt(-1).ptr);
-
-		egt(-3).ent = egt(-2).ent / egt(-1).ent;
-		exec_free_stack(2);
-		exec_return();
-	} end_function
-
-
-/**********************************************************************
-* 
-* X_MOD
-*
-**********************************************************************/
-	exec_function(X_MOD) {
-		/* -3 : n
-		 * -2 : a et val(a)
-		 * -1 : b et val(b)
-		 */
-		exec_reserve_stack(2);
-
-		egt(-2).n = container_of(egt(-3).n->c.next, struct exec_node, b);
-		egt(-1).n = container_of(egt(-2).n->b.next, struct exec_node, b);
-
-		exec_NODE(egt(-2).n, 12, egt(-2).ptr);
-		exec_NODE(egt(-1).n, 13, egt(-1).ptr);
-
-		egt(-3).ent = egt(-2).ent % egt(-1).ent;
-		exec_free_stack(2);
-		exec_return();
-	} end_function
-
-
-/**********************************************************************
-* 
-* X_EQUAL
-*
-**********************************************************************/
-	exec_function(X_EQUAL) {
-		/* -3 : n
-		 * -2 : a et val(a)
-		 * -1 : b et val(b)
-		 */
-		exec_reserve_stack(2);
-
-		egt(-2).n = container_of(egt(-3).n->c.next, struct exec_node, b);
-		egt(-1).n = container_of(egt(-2).n->b.next, struct exec_node, b);
-
-		exec_NODE(egt(-2).n, 14, egt(-2).ptr);
-		exec_NODE(egt(-1).n, 15, egt(-1).ptr);
-
-		egt(-3).ent = egt(-2).ent == egt(-1).ent;
-		exec_free_stack(2);
-		exec_return();
-	} end_function
-
-
-/**********************************************************************
-* 
-* X_STREQ
-*
-**********************************************************************/
-	exec_function(X_STREQ) {
-		/* -3 : n
-		 * -2 : a et val(a)
-		 * -1 : b et val(b)
-		 */
-		exec_reserve_stack(2);
-
-		egt(-2).n = container_of(egt(-3).n->c.next, struct exec_node, b);
-		egt(-1).n = container_of(egt(-2).n->b.next, struct exec_node, b);
-
-		exec_NODE(egt(-2).n, 43, egt(-2).ptr);
-		exec_NODE(egt(-1).n, 44, egt(-1).ptr);
-
-		egt(-3).ent = strcmp(egt(-2).string, egt(-1).string);
-		exec_free_stack(2);
-		exec_return();
-	} end_function
-
-
-/**********************************************************************
-* 
-* X_DIFF
-*
-**********************************************************************/
-	exec_function(X_DIFF) {
-		/* -3 : n
-		 * -2 : a et val(a)
-		 * -1 : b et val(b)
-		 */
-		exec_reserve_stack(2);
-
-		egt(-2).n = container_of(egt(-3).n->c.next, struct exec_node, b);
-		egt(-1).n = container_of(egt(-2).n->b.next, struct exec_node, b);
-
-		exec_NODE(egt(-2).n, 16, egt(-2).ptr);
-		exec_NODE(egt(-1).n, 17, egt(-1).ptr);
-
-		egt(-3).ent = egt(-2).ent != egt(-1).ent;
-		exec_free_stack(2);
-		exec_return();
-	} end_function
-
-
-/**********************************************************************
-* 
-* X_LT
-*
-**********************************************************************/
-	exec_function(X_LT) {
-		/* -3 : n
-		 * -2 : a et val(a)
-		 * -1 : b et val(b)
-		 */
-		exec_reserve_stack(2);
-
-		egt(-2).n = container_of(egt(-3).n->c.next, struct exec_node, b);
-		egt(-1).n = container_of(egt(-2).n->b.next, struct exec_node, b);
-
-		exec_NODE(egt(-2).n, 18, egt(-2).ptr);
-		exec_NODE(egt(-1).n, 19, egt(-1).ptr);
-
-		egt(-3).ent = egt(-2).ent < egt(-1).ent;
-		exec_free_stack(2);
-		exec_return();
-	} end_function
-
-
-/**********************************************************************
-* 
-* X_GT
-*
-**********************************************************************/
-	exec_function(X_GT) {
-		/* -3 : n
-		 * -2 : a et val(a)
-		 * -1 : b et val(b)
-		 */
-		exec_reserve_stack(2);
-
-		egt(-2).n = container_of(egt(-3).n->c.next, struct exec_node, b);
-		egt(-1).n = container_of(egt(-2).n->b.next, struct exec_node, b);
-
-		exec_NODE(egt(-2).n, 20, egt(-2).ptr);
-		exec_NODE(egt(-1).n, 21, egt(-1).ptr);
-
-		egt(-3).ent = egt(-2).ent > egt(-1).ent;
-		exec_free_stack(2);
-		exec_return();
-	} end_function
-
-
-/**********************************************************************
-* 
-* X_LE
-*
-**********************************************************************/
-	exec_function(X_LE) {
-		/* -3 : n
-		 * -2 : a et val(a)
-		 * -1 : b et val(b)
-		 */
-		exec_reserve_stack(2);
-
-		egt(-2).n = container_of(egt(-3).n->c.next, struct exec_node, b);
-		egt(-1).n = container_of(egt(-2).n->b.next, struct exec_node, b);
-
-		exec_NODE(egt(-2).n, 22, egt(-2).ptr);
-		exec_NODE(egt(-1).n, 23, egt(-1).ptr);
-
-		egt(-3).ent = egt(-2).ent <= egt(-1).ent;
-		exec_free_stack(2);
-		exec_return();
-	} end_function
-
-
-/**********************************************************************
-* 
-* X_GE
-*
-**********************************************************************/
-	exec_function(X_GE) {
-		/* -3 : n
-		 * -2 : a et val(a)
-		 * -1 : b et val(b)
-		 */
-		exec_reserve_stack(2);
-
-		egt(-2).n = container_of(egt(-3).n->c.next, struct exec_node, b);
-		egt(-1).n = container_of(egt(-2).n->b.next, struct exec_node, b);
-
-		exec_NODE(egt(-2).n, 24, egt(-2).ptr);
-		exec_NODE(egt(-1).n, 25, egt(-1).ptr);
-
-		egt(-3).ent = egt(-2).ent >= egt(-1).ent;
-		exec_free_stack(2);
-		exec_return();
-	} end_function
-
-
-/**********************************************************************
-* 
-* X_AND
-*
-**********************************************************************/
-	exec_function(X_AND) {
-		/* -3 : n
-		 * -2 : a et val(a)
-		 * -1 : b et val(b)
-		 */
-		exec_reserve_stack(2);
-
-		egt(-2).n = container_of(egt(-3).n->c.next, struct exec_node, b);
-		egt(-1).n = container_of(egt(-2).n->b.next, struct exec_node, b);
-
-		exec_NODE(egt(-2).n, 26, egt(-2).ptr);
-		exec_NODE(egt(-1).n, 27, egt(-1).ptr);
-
-		egt(-3).ent = egt(-2).ent && egt(-1).ent;
-		exec_free_stack(2);
-		exec_return();
-	} end_function
-
-
-/**********************************************************************
-* 
-* X_OR
-*
-**********************************************************************/
-	exec_function(X_OR) {
-		/* -3 : n
-		 * -2 : a et val(a)
-		 * -1 : b et val(b)
-		 */
-		exec_reserve_stack(2);
-
-		egt(-2).n = container_of(egt(-3).n->c.next, struct exec_node, b);
-		egt(-1).n = container_of(egt(-2).n->b.next, struct exec_node, b);
-
-		exec_NODE(egt(-2).n, 28, egt(-2).ptr);
-		exec_NODE(egt(-1).n, 29, egt(-1).ptr);
-
-		egt(-3).ent = egt(-2).ent || egt(-1).ent;
+		switch (egt(-3).n->type) {
+		case X_ADD:   egt(-3).ent = egt(-2).ent  + egt(-1).ent;             break;
+		case X_SUB:   egt(-3).ent = egt(-2).ent  - egt(-1).ent;             break;
+		case X_MUL:   egt(-3).ent = egt(-2).ent  * egt(-1).ent;             break;
+		case X_DIV:   egt(-3).ent = egt(-2).ent  / egt(-1).ent;             break;
+		case X_MOD:   egt(-3).ent = egt(-2).ent  % egt(-1).ent;             break;
+		case X_EQUAL: egt(-3).ent = egt(-2).ent == egt(-1).ent;             break;
+		case X_STREQ: egt(-3).ent = strcmp(egt(-2).string, egt(-1).string); break;
+		case X_DIFF:  egt(-3).ent = egt(-2).ent != egt(-1).ent;             break;
+		case X_LT:    egt(-3).ent = egt(-2).ent  < egt(-1).ent;             break;
+		case X_GT:    egt(-3).ent = egt(-2).ent  > egt(-1).ent;             break;
+		case X_LE:    egt(-3).ent = egt(-2).ent <= egt(-1).ent;             break;
+		case X_GE:    egt(-3).ent = egt(-2).ent >= egt(-1).ent;             break;
+		case X_AND:   egt(-3).ent = egt(-2).ent && egt(-1).ent;             break;
+		case X_OR:    egt(-3).ent = egt(-2).ent || egt(-1).ent;             break;
+		/* just for warnings ! */
+		case X_NULL:
+		case X_COLLEC:
+		case X_PRINT:
+		case X_ASSIGN:
+		case X_DISPLAY:
+		case X_FUNCTION:
+		case X_VAR:
+		case X_INTEGER:
+		case X_STRING:
+		case X_SWITCH:
+		case X_FOR:
+		case X_WHILE:
+		case X_IF:
+		case X_BREAK:
+		case X_CONT:
+		default:
+			snprintf(r->e->error, ERROR_LEN, 
+			         "[%s:%d] unknown function code", __FILE__, __LINE__);
+			return -1;
+		}
 		exec_free_stack(2);
 		exec_return();
 	} end_function
